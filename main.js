4464a3f1f619e3232d6483b51667c0302e5a3fc1
Moczała Maciej <moczalamaciej@MacBook-Pro-Moczaa.local>
2019-10-07 09:27:28 +0200

    Added type-header-on-scroll feature

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
+//
+//  main.js
+//
+//  Created on 6.10.2019 by Matt Moczala
+//
+
+// I know this is spaghetti code, that sucks. 
+// All I can say is: I was in fuckin' hurry. 
+
+
 let positions = ['.nav', '.about', '.projects', '.footer'];
 let currPos = 0;
+let aboutTyped = false,
+    projectsTyped = false;
 
+
+// scrollTop before load, to make sure website is at top
 window.onbeforeunload = function() {
     window.scrollTo(0, 0);
 }
 
+// Initialize some scripts
 window.onload = () => {
 
+    AOS.init({
+        once: true
+    });
     initTypeWriter();
     setAuthorPhotoSize();
     // initScroll();
@@ -15,33 +33,96 @@ window.onload = () => {
 
 }
 
+// Checks if element is contained in user's viewport
+function isScrolledIntoView(elem) {
+    let docViewTop = $(window).scrollTop();
+    let docViewBottom = docViewTop + $(window).height();
+    let elemTop = $(elem).offset().top;
+    let elemBottom = elemTop + $(elem).height();
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+// When users scrolls to section, start typing it's header
+$(window).scroll(function() {
+
+    if (isScrolledIntoView($('#aboutmeHeader')) && !aboutTyped) {
+        //the div is now visible to user. 
+        aboutTyped = true;
+
+        let app = document.getElementById('aboutmeHeader');
+
+        let typewriter = new Typewriter(app, {
+            loop: false,
+            cursorClassName: 'aboutMeCursor'
+        });
+
+        typewriter.typeString('about me')
+            .callFunction(() => {
+                setInterval(() => {
+                    $('.aboutMeCursor').hide();
+                }, 300);
+            })
+            .start();
+
+    } else if (isScrolledIntoView($('#projectsHeader')) && !projectsTyped) {
+        //the div is now visible to user. 
+        projectsTyped = true;
+
+        aboutTyped = true;
+
+        let app = document.getElementById('projectsHeader');
+
+        let typewriter = new Typewriter(app, {
+            loop: false,
+            cursorClassName: 'projectsCursor'
+        });
+
+        typewriter.typeString('projects')
+            .callFunction(() => {
+                setInterval(() => {
+                    $('.projectsCursor').hide();
+                }, 300);
+            })
+            .start();
+
+    }
+});
+
+// setSize of margins in felxbox, css had some issues, so here is a quick fix
 function setAuthorPhotoSize() {
     let warpperHeight = document.querySelector('.photo').clientHeight;
     let photoHeight = document.querySelector('.authorPhoto').clientHeight;
 
     let margin = (warpperHeight - photoHeight) / 2;
-    // 30 is average size of social icons
+    // 30 is a size of social icons
     document.getElementById('myPhoto').style.marginTop = `${margin-30}px`;
 
+    let wrapperHeight = document.querySelector('.aboutWrapper').clientHeight;
+    let aboutHeight = document.querySelector('.about').clientHeight;
+    let resumeHeight = document.querySelector('.resumeWrapper').clientHeight;
+
+    let quoteMargin = (wrapperHeight - aboutHeight - resumeHeight) / 2;
+    console.log(quoteMargin, wrapperHeight, aboutHeight, resumeHeight);
+    document.querySelector('.quoteWrapper').style.marginTop = `${quoteMargin-30}px`;
 
 }
 
+// Enable scroll on arrow-keys
 function checkKey(e) {
     e = e || window.event;
 
     if (e.keyCode == '38') {
+        // UP
         e.preventDefault();
-        console.log('up');
         if (currPos > 0) {
             currPos--;
-            // console.log(positions[currPos]);
             document.querySelector(positions[currPos]).scrollIntoView({
-                behavior: 'smooth'
+                behavior: 'smooth',
             });
         }
     } else if (e.keyCode == '40') {
+        // DOWN
         e.preventDefault();
-        console.log('down');
         if (currPos < 3) {
             currPos++;
             // console.log(positions[currPos]);
@@ -53,6 +134,7 @@ function checkKey(e) {
     }
 }
 
+// Typewriter in main section
 function initTypeWriter() {
     let app = document.getElementById('app');
 
@@ -78,6 +160,7 @@ function initTypeWriter() {
         .start();
 }
 
+// Garbage, might delete it later
 function initScroll() {
     let scrollableElement = document.body;
 
